8b72d1d9f61dfc016019b0c9e48d0f61adfb07cd
Bettercook <2193458657@qq.com>
2026-03-23 17:30:20 +0800

    初始化

---
+++ Assets/Scripts/UI/BasePanel.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasePanel : MonoBehaviour
+{
+    //专门用于控制面板透明度的组件
+    private CanvasGroup canvasGroup;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
